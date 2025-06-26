15818bd54580149090abf969b1e9c64bcaa0cec3
tariqyassir6 <tariqyassir6@gmail.com>
2025-06-26 15:47:26 +0100

    Third commit

--- cypress/e2e/SearchTest.cy.js
+++ cypress/e2e/SearchTest.cy.js
@@ -1,40 +1,40 @@
 import {selectDate} from '../support/utils'
 import {selectGuests} from '../support/utils'
+import {searchCity} from '../support/utils'
 
 
 
 
-describe('Home page load and navigatin check', () => {  
+
+describe('Search results', () => {  
 //=========================================================================================================================================================================
 
-    it('should apply a filter and update results', () => {
+    it('Results page loads', () => {
 
         cy.visit("https://www.booking.com/")
 
-        //Type “Paris” in the destination field.
-        cy.get("input[id=':rh:']").type("Paris")
-
-        //close a sign in popup
-        cy.wait(2000)
-        cy.get('body').then($body => {
-        const selector = '.b779265b5e > .e1e158e66b > .de576f5064';
-
-        if ($body.find(selector).length > 0) {
-            cy.get(selector).click();
-        } else {
-            cy.log('Popup not found, skipping close.');
-         }
-            });
-
-        //Choose valid check-in and check-out dates.
+        
+        searchCity("Paris")
+        
         selectDate("2025-07-01")
         selectDate("2025-07-03")
         
-        //selecting 4 adults , then 3 kids with ages 3,4 and 5 then slecting 2 rooms
+        
         selectGuests( {adults : 4 , kids : 3 , kidAges : [3,4,5] , rooms : 2})
 
-        //Click the search button.
+        //getting the search request
+        cy.intercept('GET', '**/searchresults*').as('searchRequest')
+
+        //click on the search button
         cy.get("button[type='submit']").click()
+
+
+        //check if the body is not empty and the satus code is equal to 200
+
+        cy.wait('@searchRequest').then((interception) => {
+            expect(interception.response.body).to.not.be.empty
+            expect(interception.response.statusCode).to.eq(200)
+})
 
     
     })
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -86,3 +86,24 @@ export function selectGuests({adults = 1  , kids = 0 , kidAges = [] , rooms = 1 
 
 
 }
+
+
+
+//========================================================================================================================================
+export function searchCity(city) {
+
+    cy.get("input[id=':rh:']").type(city)
+    //wait to close pop up
+    cy.wait(3000)
+        cy.get('body').then($body => {
+        const selector = '.b779265b5e > .e1e158e66b > .de576f5064';
+
+        if ($body.find(selector).length > 0) {
+            cy.get(selector).click();
+        } else {
+            cy.log('Popup not found, skipping close.');
+         }
+            })
+
+}
+
